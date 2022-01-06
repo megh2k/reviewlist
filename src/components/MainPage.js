08753421277a72cfb8921d1ad094ecbdbeb3bbd3
Megh Patel <86826735+megh2k@users.noreply.github.com>
2022-01-06 16:16:36 -0500

    carousel aadded

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,23 +3,6 @@ import {Carousel} from "react-bootstrap"
 
 function MainPage(props){
 
-    const responsive = {
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3,
-          slidesToSlide: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2,
-          slidesToSlide: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1,
-          slidesToSlide: 1
-        }
-      };
     return(
         <Carousel variant="white">
             {props.list.map(x =>{
